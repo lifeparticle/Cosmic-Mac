1558b1dff19a0f8a8c5f06529e8a48fb3076e1fe
genesis <mahbubzaman17@gmail.com>
2019-06-02 22:03:01 +1000

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+# Cosmic-Mac
+
+This script will install google chrome, java, android studio, sublime text, MAMP, Dropbox and apply configurations on your Mac.
+
+## Prerequisite
+[Cosmic-Terminal](https://github.com/lifeparticle/Cosmic-Terminal)
+```
+curl -s https://raw.githubusercontent.com/lifeparticle/Cosmic-Terminal/master/install_cosmic_terminal.sh | sh
+```
+or install [brew](https://brew.sh)
+```
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+````
+
+## Install
+
+Make install_cosmic_mac.sh executable
+```
+chmod +x install_cosmic_mac.sh
+```
+
+Run
+```
+./install_cosmic_mac.sh
+```
+
+or
+
+```
+curl -s https://raw.githubusercontent.com/lifeparticle/Cosmic-Mac/master/install_cosmic_mac.sh | sh
+```
+## Mac Configurations
+```
+defaults help
+
+defaults domains
+defaults domains | tr ',' '\n'
+defaults read com.apple.finder
+
+defaults read > before
+defaults read > after
+
+diff before after
+```
+
+## Tested OS
+1. macOS High Sierra (version 10.13.6)
+
+## Bug Reports and Feature Requests
+Please create an issue with as much information you can. Thank you.
+
+## Author
+Mahbub Zaman (https://mahbub.ninja)
+
+## License
+MIT License
---
+++ install_cosmic_mac.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+brew cask install google-chrome
+
+brew cask install java
+
+# not working ?
+# brew cask android-studio
+
+brew cask install sublime-text
+
+brew cask install dash
+
+brew cask install dropbox
+
+brew cask install mamp
+
+# For Dashboard
+
+# dashboard-enabled-state = 1 [off]
+# dashboard-enabled-state = 2 [As space]
+# dashboard-enabled-state = 3 [As Overlay]
+defaults write com.apple.dashboard dashboard-enabled-state -int 3
+
+# killall Dock
+
+# For Dock
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Hot corner > Bottom right screen corner > Dashboard
+defaults write com.apple.dock wvous-br-corner -int 7
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# killall Dock
+
+# For Finder
+
+# show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Finder > View > As Columns
+# As Icons = icnv
+# As List = Nlsv
+# As Cover Flow = Flwv
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+# ShowExternalHardDrivesOnDesktop
+# ShowHardDrivesOnDesktop
+# ShowPathbar
+# ShowPreviewPane
+# ShowRemovableMediaOnDesktop
+# ShowSidebar
+# ShowStatusBar
+# ShowTabView
+
+# Finder > View > Show Status Bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Finder > View > Show Tab Bar
+defaults write com.apple.finder ShowTabView -bool true
+
+# killall Finder
+
+# For menue bar
+
+defaults write com.apple.systemuiserver menuExtras -array \
+  "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+  "/System/Library/CoreServices/Menu Extras/Battery.menu" \
+  "/System/Library/CoreServices/Menu Extras/Volume.menu"
+  
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -bool true
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.battery" -bool true
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.volume" -bool true
+
+# killall SystemUIServer
+        
+# Kill affected apps
+for app in "Dock" "Finder" "SystemUIServer"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+echo "You may need to restart your machine."
